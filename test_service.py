84c7064743fba9a7ee3e2b0138545819ebcdf912
Olamide Omolola <omololaolamidex@gmail.com>
2025-11-14 00:07:59 +0100

    update

--- test_service.py
+++ test_service.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:8000/predict"
+url = "http://0.0.0.0:9696/predict"
 client = {"area": 3290, "bedrooms": 2, "bathrooms": 1, "stories": 1, "mainroad": "yes", "guestroom": "no", "basement": "no",
  "hotwaterheating": "yes", "airconditioning": "no", "parking": 1, "prefarea": "no", "furnishingstatus": "furnished"}
 
