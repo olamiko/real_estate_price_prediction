4ef8c488a618b7c88023504da2c24569209b91c2
Olamide Omolola <omololaolamidex@gmail.com>
2025-11-15 00:14:30 +0100

    further updates

--- README.md
+++ README.md
@@ -1,6 +1,71 @@
-# real_estate_price_prediction
-A model for predicting how the price of a house based on a kaggle dataset.
+# Real Estate Price Prediction
 
-## Problem Description
-One of the most frequent problems in the real estate sector is figuring out what influences property values.
-This dataset is used to construct prediction models that forecast property prices based on particular qualities and provides important insights into the housing market.
+![Python](https://img.shields.io/badge/python-3.10%2B-blue)
+![License](https://img.shields.io/badge/license-MIT-green)
+![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=flat&logo=docker&logoColor=white)
+![FastAPI](https://img.shields.io/badge/FastAPI-005571?style=flat?style=flat&logo=fastapi)
+
+A **machine learning model** to predict **real estate prices** using the **[Real Estate Price Insights](https://www.kaggle.com/datasets/wardabilal/real-estate-price-insights)** dataset. Deployed as a **FastAPI service** in **Docker**, managed with **uv** for fast, reliable dependency resolution.
+
+---
+
+## Problem Statement & Motivation
+
+> **"What drives the price of a home?"**
+
+In an increasingly competitive and volatile real estate market, **accurate price prediction** is essential for:
+
+- **Buyers**: Avoid overpaying in hot markets
+- **Sellers**: Price competitively to attract offers
+- **Investors**: Identify undervalued properties
+- **Lenders & Insurers**: Assess risk with data-driven valuations
+- **Policymakers**: Monitor housing affordability trends
+
+Traditional valuation relies on manual appraisals — **slow, costly, and inconsistent**.
+
+This project uses **machine learning** to:
+- Predict **property sale prices** from structural and locational features
+- Quantify **feature importance** (e.g., sqft, bedrooms)
+- Deliver **instant, scalable predictions** via API
+
+---
+
+## Dataset
+
+**Source**: [Real Estate Price Insights](https://www.kaggle.com/datasets/wardabilal/real-estate-price-insights)
+**Download**:
+```bash
+kaggle datasets download -d wardabilal/real-estate-price-insights -p data/raw --unzip
+```
+
+---
+## Deployment And Containerization
+
+The model is deployed as a FastAPI application and as a docker container. 
+The file called `predict.py` contains the core logic of web service. 
+The app itself is served using uvicorn.
+
+
+**Run FastAPI application:** 
+
+Install [UV](https://docs.astral.sh/uv/getting-started/installation/#standalone-installer) 
+
+```
+uv sync --locked
+uv run uvicorn --bind 0.0.0.0:9696 predict:app
+```
+
+
+**Run application as a docker container:**
+```
+docker build -t real-estate-price-prediction .
+docker run -it --rm -p 9696:9696 real-estate-price-prediction
+```
+
+---
+## Testing
+
+A test script is provided. Modify as needed:
+```
+uv run test-service.py
+```
---
+++ train.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+
+import seaborn as sns
+from matplotlib import pyplot as plt
+import pickle
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression
+
+# Dataset gotten from 
+# https://www.kaggle.com/api/v1/datasets/download/wardabilal/real-estate-price-insights
+
+df = pd.read_csv('housing_price_data.csv')
+df.isna().sum()
+df.dtypes
+
+for col in df.columns:
+    print(col)
+    print(df[col].unique()[:5])
+    print(df[col].nunique())
+    print()
+
+# Check the target variable
+
+sns.histplot(df.price, bins=50)
+
+# We will need to use a log distribution due to the shape of the distribution
+
+price_logs = np.log1p(df.price)
+sns.histplot(price_logs, bins=50)
+
+# Now let's split the data
+
+df_full_train, df_test = train_test_split(df, test_size=0.2, random_state=25)
+df_train, df_val = train_test_split(df_full_train, test_size=0.25, random_state=25)
+
+len(df_train), len(df_val), len(df_test)
+
+df_train = df_train.reset_index(drop=True)
+df_test = df_test.reset_index(drop=True)
+df_val = df_val.reset_index(drop=True)
+
+y_train = np.log1p(df_train.price.values)
+y_test = np.log1p(df_test.price.values)
+y_val = np.log1p(df_val.price.values)
+
+del df_train['price']
+del df_test['price']
+del df_val['price']
+
+train_dicts = df_train.to_dict(orient='records')
+val_dicts = df_val.to_dict(orient='records')
+
+dv = DictVectorizer(sparse=False)
+
+X_train = dv.fit_transform(train_dicts)
+X_val = dv.transform(val_dicts)
+
+# Correlate with price
+
+numerical = ['area','bedrooms','bathrooms','stories', 'parking']
+df_full_train[numerical].corrwith(df_full_train.price)
+
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_val)
+
+def rmse(y, y_pred):
+    error = y - y_pred
+    se = error ** 2
+    mse = se.mean()
+    return np.sqrt(mse)
+
+print("The RMSE using Linear Regression is: ", rmse(y_val, y_pred))
+
+# Implement XGBoost
+import xgboost as xgb
+
+def tune_xgbregressor(x_train, x_test, max_depth=2, n_estimators=10, learning_rate=0.2):
+    model = xgb.XGBRegressor(objective='reg:squarederror', 
+                             max_depth=max_depth, 
+                             learning_rate=learning_rate,
+                             n_estimators=n_estimators,
+                             random_state=42)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_val)
+    return rmse(y_val, y_pred)
+
+# Tune learning rate
+learning_rate = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+
+print("Learning rate results")
+for l in learning_rate:
+    print("{} -> {}".format(l,tune_xgbregressor(X_train, X_val, learning_rate=l)))
+
+# Fix learning_rate
+learning_rate = 0.5
+
+# Tune max_depth
+max_depth = [2,5,6,10,15,20]
+
+print("Max depth results")
+for m in max_depth:
+    print("{} -> {}".format(m, tune_xgbregressor(X_train, X_val, 
+                                                learning_rate=learning_rate,
+                                                max_depth=m)))
+
+# Fix max_depth
+max_depth = 2
+
+
+# Tune n_estimators
+n_estimators = [6,10,15,20,50,100]
+
+print("Number of estimators results")
+for n in n_estimators:
+    print("{} -> {}".format(n, tune_xgbregressor(X_train, X_val, 
+                                                learning_rate=learning_rate,
+                                                max_depth=max_depth,
+                                                n_estimators=n)))
+
+
+# Fix n_estimators
+n_estimators = 20
+
+
+# Train model with full train
+# We will use xgb since it is slightly better than linear regression
+
+y_full_train = np.log1p(df_full_train.price.values)
+
+del(df_full_train['price'])
+
+
+dicts_full_train = df_full_train.to_dict(orient='records')
+dicts_test = df_test.to_dict(orient='records')
+
+pipeline = make_pipeline(
+    DictVectorizer(),
+    LinearRegression()
+)
+
+pipeline.fit(dicts_full_train, y_full_train)
+
+y_pred = pipeline.predict(dicts_test)
+print("The RMSE for final Linear Regression Model is: ", rmse(y_test, y_pred))
+
+dicts_full_train = df_full_train.to_dict(orient='records')
+dicts_test = df_test.to_dict(orient='records')
+
+pipeline = make_pipeline(
+    DictVectorizer(),
+    xgb.XGBRegressor(objective='reg:squarederror', 
+                         max_depth=max_depth, 
+                         learning_rate=learning_rate,
+                         n_estimators=n_estimators,
+                         random_state=42)
+)
+
+pipeline.fit(dicts_full_train, y_full_train)
+
+y_pred = pipeline.predict(dicts_test)
+print("The RMSE for final XGB Regression Model is: ", rmse(y_test, y_pred))
+
+output_file = 'pipeline.bin'
+
+with open(output_file, 'wb') as f_out:
+    pickle.dump(pipeline, f_out)
+
+
+print(f'the model is saved to {output_file}')
+
+# df_full_train.iloc[30].to_dict()
+
+# np.expm1(y_full_train[30])
+
